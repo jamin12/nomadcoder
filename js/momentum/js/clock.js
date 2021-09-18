db31ba6ea3dc99420c3e63b6b60847d316bdd000
jamin12 <rudals9901@naver.com>
2021-09-18 23:12:15 +0900

    padstart

--- js/momentum/js/clock.js
+++ js/momentum/js/clock.js
@@ -2,7 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    // padstart 00 01 -> 만들기
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 
